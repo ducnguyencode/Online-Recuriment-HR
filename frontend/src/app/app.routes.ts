9fe27b5ed0a4c1f8fe6bc035d2037ad9ed6d3e9c
Nguyễn Phúc Nguyên Khang <nguyenpnguyenkhang2412@gmail.com>
2026-05-06 21:49:21 +0700

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -18,12 +18,8 @@ export const routes: Routes = [
   {
     path: 'hr/login',
     component: LoginComponent,
-<<<<<<< Updated upstream
-    data: { role: 'HR' }
-=======
     data: { role: 'HR' },
     canActivate: [loginGuard],
->>>>>>> Stashed changes
   },
   {
     path: '',
